00eb6eb5efe7d4fd6476db021722433ab32518e9
AstroExplorer <astro@explorer.com>
2026-07-05 21:54:42 +0800

    feat: init glassmorphism astro blog

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'AstroExplorer の 宝藏之地';
+export const SITE_DESCRIPTION = '在代码、学术与数码艺术中穿梭的旅人。';
